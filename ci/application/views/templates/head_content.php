e11f088103debc4cfef06bd27450e73ed07d0c01
AllanJeremy <aj.dev254@gmail.com>
2018-06-26 06:27:30 +0300

    [Bugfix] Favicon correctly displays now - Moved favicon to assets folder

--- ci/application/views/templates/head_content.php
+++ ci/application/views/templates/head_content.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= isset($page_title) ? $page_title : ucwords(SITE_TITLE) ;?></title>
+<link rel="icon" type="img/ico" href="<?= get_asset_url('favicon.ico');?>">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/4.1.1/materia/bootstrap.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
 <link rel="stylesheet" href="<?= get_asset_url('css/styles.min.css'); ?>">
